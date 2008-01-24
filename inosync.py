b78b583e0e96db94fbf789f78a4f0f5e3a2b9c61
Benedikt Böhm <bb@xnull.de>
2008-01-24 22:24:37 +0000

    commit version 0.2
    git-svn-id: https://svn.xnull.de/svn/misc/inosync@3 64f59ecd-8d50-41f5-9da3-17d56c670b69

--- inosync.py
+++ inosync.py
@@ -1,47 +1,132 @@
 #!/usr/bin/python
 # vim: set fileencoding=utf-8 ts=2 sw=2 expandtab :
+#
+# Copyright (c) 2007-2008 Benedikt Böhm
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# ChangeLog:
+#
+# *inosync-0.2 (24 Jan 2008):
+#   use configuration files
+#   add event delay option
+#
+# *inosync-0.1 (09 Jan 2008):
+#   initial version
 
-import os,sys,resource
-from optparse import OptionParser
+import os,sys
+from optparse import OptionParser,make_option
+from time import sleep
 from syslog import *
 from pyinotify import *
 
 __author__ = "Benedikt Böhm"
-__copyright__ = "Copyright (C) 2007 Benedikt Böhm"
-__version__ = "0.1"
+__copyright__ = "Copyright (c) 2007-2008 Benedikt Böhm"
+__version__ = 0,2
+
+OPTION_LIST = [
+  make_option(
+      "-c", dest = "config",
+      default = "/etc/inosync/default.py",
+      metavar = "FILE",
+      help = "load configuration from FILE"),
+  make_option(
+      "-d", dest = "daemonize",
+      action = "store_true",
+      default = False,
+      help = "daemonize %prog"),
+  make_option(
+      "-p", dest = "pretend",
+      action = "store_true",
+      default = False,
+      help = "do not actually call rsync"),
+  make_option(
+      "-v", dest = "verbose",
+      action = "store_true",
+      default = False,
+      help = "print debugging information"),
+]
+
+ALL_EVENTS = [
+    "IN_ACCESS",
+    "IN_ATTRIB",
+    "IN_CLOSE_WRITE",
+    "IN_CLOSE_NOWRITE",
+    "IN_CREATE",
+    "IN_DELETE",
+    "IN_DELETE_SELF",
+    "IN_MODIFY",
+    "IN_MOVED_FROM",
+    "IN_MOVED_TO",
+    "IN_OPEN"
+]
+
+DEFAULT_EVENTS = [
+    "IN_CLOSE_WRITE",
+    "IN_CREATE",
+    "IN_DELETE",
+    "IN_MOVED_FROM",
+    "IN_MOVED_TO"
+]
 
 class RsyncEvent(ProcessEvent):
-  options = None
+  pretend = None
+  dirty = True
 
-  def __init__(self, options):
-    self.options = options
+  def __init__(self, pretend=False):
+    self.pretend = pretend
 
   def sync(self):
-    args = [self.options.rsync_path, "-ltrp", "--delete"]
-    if self.options.verbose:
-      args.append("-v")
-    if self.options.exclude:
-      args.append("--exclude-from=%s" % self.options.exclude)
-    args.append(self.options.dir)
+    if not self.dirty:
+      return
+    args = [config.rsync, "-ltrp", "--delete"]
+    args.append("--bwlimit=%s" % config.rspeed)
+    if "excludes" in dir(config):
+      for exclude in config.excludes:
+        args.append("--exclude=%s" % exclude)
+    args.append(config.wpath)
     args.append("%s")
     cmd = " ".join(args)
-    for remote in self.options.remotes:
-      if self.options.pretend:
-        syslog("would execute `%s'" % (cmd % remote))
+    for node in config.rnodes:
+      if self.pretend:
+        syslog("would execute `%s'" % (cmd % node))
       else:
-        syslog(LOG_DEBUG, "executing %s" % (cmd % remote))
-        proc = os.popen(cmd % remote)
+        syslog(LOG_DEBUG, "executing %s" % (cmd % node))
+        proc = os.popen(cmd % node)
         for line in proc:
           syslog(LOG_DEBUG, "[rsync] %s" % line.strip())
+    self.dirty = False
 
   def process_default(self, event):
     syslog(LOG_DEBUG, "caught %s on %s" % \
         (event.event_name, os.path.join(event.path, event.name)))
-    if not event.event_name in self.options.events:
+    if not event.event_name in config.emask:
       syslog(LOG_DEBUG, "ignoring %s on %s" % \
           (event.event_name, os.path.join(event.path, event.name)))
       return
-    self.sync()
+    self.dirty = True
 
 def daemonize():
   try:
@@ -69,108 +154,61 @@ def daemonize():
 
   return 0
 
-def parse_options():
-  usage = "usage: %prog [options]"
-  parser = OptionParser(usage, version = __version__)
+def load_config(filename):
+  if not os.path.isfile(filename):
+    raise RuntimeError, "configuration file does not exist: %s" % filename
 
-  parser.add_option(
-      "-d", "--daemon",
-      action = "store_true",
-      dest = "daemonize", default = False,
-      help = "daemonize %prog")
-  parser.add_option(
-      "-e", "--exclude-from",
-      dest = "exclude",
-      metavar = "FILE",
-      help = "exclude directories listed in FILE")
-  parser.add_option(
-      "-m", "--mask",
-      action = "append",
-      dest = "events",
-      metavar = "EVENT",
-      help = "synchronize on inotify event EVENT")
-  parser.add_option(
-      "-p", "--pretend",
-      action = "store_true",
-      dest = "pretend", default = False,
-      help = "do not actually call rsync")
-  parser.add_option(
-      "-r", "--remote",
-      action = "append",
-      dest = "remotes",
-      metavar = "DEST",
-      help = "synchronize to (rsync-compatible) DEST")
-  parser.add_option(
-      "--rsync-path",
-      default = "/usr/bin/rsync",
-      metavar = "PATH",
-      help = "PATH to the rsync binary")
-  parser.add_option(
-      "-v", "--verbose",
-      action = "store_true",
-      dest = "verbose", default = False,
-      help = "print debugging information")
-  parser.add_option(
-      "-w", "--watch",
-      dest = "dir",
-      metavar = "DIR",
-      help = "watch DIR for changes")
+  configdir  = os.path.dirname(filename)
+  configfile = os.path.basename(filename)
 
+  if configfile.endswith(".py"):
+    configfile = configfile[0:-3]
+
+  sys.path.append(configdir)
+  exec("import %s as __config__" % configfile)
+  sys.path.remove(configdir)
+
+  global config
+  config = __config__
+
+  if not "wpath" in dir(config):
+    raise RuntimeError, "no watch path given"
+  if not os.path.isdir(config.wpath):
+    raise RuntimeError, "watch path does not exist: %s" % config.wpath
+  if not os.path.isabs(config.wpath):
+    config.wpath = os.path.abspath(config.wpath)
+
+  if not "rnodes" in dir(config) or len(config.rnodes) < 1:
+    raise RuntimeError, "no remote nodes given"
+
+  if not "rspeed" in dir(config) or config.rspeed < 0:
+    config.rspeed = 0
+
+  if not "emask" in dir(config):
+    config.emask = DEFAULT_EVENTS
+  for event in config.emask:
+    if not event in ALL_EVENTS:
+      raise RuntimeError, "invalid inotify event: %s" % event
+
+  if not "edelay" in dir(config):
+    config.edelay = 10
+  if config.edelay < 1:
+    raise RuntimeError, "event delay needs to be greater than 1"
+
+  if not "rsync" in dir(config):
+    config.rsync = "/usr/bin/rsync"
+  if not os.path.isabs(config.rsync):
+    raise RuntimeError, "rsync path needs to be absolute"
+  if not os.path.isfile(config.rsync):
+    raise RuntimeError, "rsync binary does not exist: %s" % config.rsync
+
+def main():
+  version = ".".join(map(str, __version__))
+  parser = OptionParser(option_list=OPTION_LIST,version="%prog " + version)
   (options, args) = parser.parse_args()
 
   if len(args) > 0:
     parser.error("too many arguments")
-
-  if options.dir == None:
-    parser.error("no watch directory given")
-  if not os.path.isdir(options.dir):
-    parser.error("directory does not exist: %s" % options.dir)
-  if not os.path.isabs(options.dir):
-    options.dir = os.path.abspath(options.dir)
-
-  if options.remotes == None:
-    parser.error("no remote location(s) specified")
-
-  if options.exclude and not os.path.isfile(options.exclude):
-    parser.error("exclude file does not exist: %s" % options.exclude)
-  if options.exclude and not os.path.isabs(options.exclude):
-    options.exclude = os.path.abspath(options.exclude)
-
-  if not os.path.isabs(options.rsync_path):
-    parser.error("rsync path needs to be absolute")
-
-  all_events = [
-      "IN_ACCESS",
-      "IN_ATTRIB",
-      "IN_CLOSE_WRITE",
-      "IN_CLOSE_NOWRITE",
-      "IN_CREATE",
-      "IN_DELETE",
-      "IN_DELETE_SELF",
-      "IN_MODIFY",
-      "IN_MOVED_FROM",
-      "IN_MOVED_TO",
-      "IN_OPEN"
-  ]
-
-  default_events = [
-      "IN_CLOSE_WRITE",
-      "IN_CREATE",
-      "IN_DELETE",
-      "IN_MOVED_FROM",
-      "IN_MOVED_TO"
-  ]
-
-  if not options.events:
-    options.events = default_events
-  for event in options.events:
-    if not event in all_events:
-      parser.error("invalid inotify event: %s" % event)
-
-  return options
-
-def main():
-  options = parse_options()
 
   logopt = LOG_PID|LOG_CONS
   if not options.daemonize:
@@ -181,27 +219,29 @@ def main():
   else:
     setlogmask(LOG_UPTO(LOG_INFO))
 
-  syslog(LOG_DEBUG, "event mask is %s" % options.events)
+  load_config(options.config)
 
   if options.daemonize:
     daemonize()
 
   wm = WatchManager()
-  ev = RsyncEvent(options)
+  ev = RsyncEvent(options.pretend)
   notifier = Notifier(wm, ev)
-  wds = wm.add_watch(options.dir, EventsCodes.ALL_EVENTS,
+  wds = wm.add_watch(config.wpath, EventsCodes.ALL_EVENTS,
       rec = True, auto_add = True)
 
-  syslog(LOG_DEBUG, "starting initial synchronization on %s" % options.dir)
+  syslog(LOG_DEBUG, "starting initial synchronization on %s" % config.wpath)
   ev.sync()
-  syslog(LOG_DEBUG, "initial synchronization on %s done" % options.dir)
+  syslog(LOG_DEBUG, "initial synchronization on %s done" % config.wpath)
 
-  syslog("resuming normal operations on %s" % options.dir)
+  syslog("resuming normal operations on %s" % config.wpath)
   while True:
     try:
       notifier.process_events()
-      if notifier.check_events():
+      if notifier.check_events(0):
         notifier.read_events()
+      ev.sync()
+      sleep(config.edelay)
     except KeyboardInterrupt:
       notifier.stop()
       break
---
+++ sample_config.py
@@ -0,0 +1,36 @@
+# directory that should be watched for changes
+wpath = "/var/www/"
+
+# exclude list for rsync
+rexcludes = [
+	"/localhost",
+]
+
+# common remote path
+rpath = "/var/www/"
+
+# remote locations in rsync syntax
+rnodes = [
+	"a.mirror.com:" + rpath,
+	"b.mirror.com:" + rpath,
+	"c.mirror.com:" + rpath,
+]
+
+# limit remote sync speed (in KB/s, 0 = no limit)
+#rspeed = 0
+
+# event mask (only sync on these events)
+#emask = [
+#	"IN_CLOSE_WRITE",
+#	"IN_CREATE",
+#	"IN_DELETE",
+#	"IN_MOVED_FROM",
+#	"IN_MOVED_TO",
+#]
+
+# event delay in seconds (prevents huge amounts of syncs, but dicreases the
+# realtime side of things)
+#edelay = 10
+
+# rsync binary path
+#rsync = "/usr/bin/rsync"
