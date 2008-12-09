bad791e5f23cb4dcd549e68ef6c42dc25d6cbee5
Benedikt Böhm <bb@xnull.de>
2008-12-09 13:06:01 +0100

    add README, LICENSE and release script

--- inosync.py
+++ inosync.py
@@ -1,40 +1,5 @@
 #!/usr/bin/python
 # vim: set fileencoding=utf-8 ts=2 sw=2 expandtab :
-#
-# Copyright (c) 2007-2008 Benedikt Böhm <hollow@gentoo.org>
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# ChangeLog:
-#
-# *inosync-0.2 (24 Jan 2008):
-#   use configuration files
-#   add event delay option
-#
-# *inosync-0.1 (09 Jan 2008):
-#   initial version
 
 import os,sys
 from optparse import OptionParser,make_option
@@ -43,7 +8,7 @@ from syslog import *
 from pyinotify import *
 
 __author__ = "Benedikt Böhm"
-__copyright__ = "Copyright (c) 2007-2008 Benedikt Böhm <hollow@gentoo.org>"
+__copyright__ = "Copyright (c) 2007-2008 Benedikt Böhm <bb@xnull.de>"
 __version__ = 0,2
 
 OPTION_LIST = [
